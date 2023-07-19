451cc5264be2038b351a384fb1da3778b3c087b4
Margarita Montenegro <margamontv@gmail.com>
2023-07-19 17:22:24 +0200

    project updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 st.set_page_config(
-        page_title="Book Recommending APP",
+        page_title="BOOK RECOMMENDING APP",
 )
 
 # Cargar el CSS personalizado
@@ -22,7 +22,7 @@ title_image = "./book_logo_title.jpg"
 
 # Display the image as the app title
 st.image(title_image)
-st.title("Book Recommending APP")
+st.title("BOOK RECOMMENDING APP")
 
 st.markdown("""
  * Use the left menu to select your books preferences
@@ -32,37 +32,37 @@ st.markdown("""
 #Filter 1
 genre = st.sidebar.selectbox(
     'Which genre would you like to read?',
-    ('Adult Fiction', 'Biography', 'Business', 'Chick-Lit', 'Childrens', 
-              'Comics', 'Contemporary', 'Crime', 'Fantasy', 'Fiction', 
-              'Graphic Novels', 'Historical Fiction', 'History', 'Horror', 'Humor', 
-              'LGBT', 'Literary Fiction', 'Memoir', 'Mystery', 'New Adult', 
-              'Nonfiction', 'Paranormal', 'Poetry', 'Romance', 'Science', 
-              'Science Fiction', 'Self-Help', 'Suspense', 'Thriller', 'Travel', 'Young Adult'))
-df_genre = books_df[books_df['Genre'] == genre]
+    ("Adult Fiction", "Biography", "Business", "Chick-Lit", "Childrens", 
+              "Comics", "Contemporary", "Crime", "Fantasy", "Fiction", 
+              "Graphic Novels", "Historical Fiction", "History", "Horror", "Humor", 
+              "LGBT", "Literary Fiction", "Memoir", "Mystery", "New Adult", 
+              "Nonfiction", "Paranormal", "Poetry", "Romance", "Science", 
+              "Science Fiction", "Self-Help", "Suspense", "Thriller", "Travel", "Young Adult"))
+df_genre = books_df[books_df["Genre"] == genre]
 
 #Filter 2
-pages = st.sidebar.selectbox('Would you like to read a short book or a long book?',
-                             ('Short', 'Long'))
+pages = st.sidebar.selectbox("Would you like to read a short book or a long book?",
+                             ("Short", "Long"))
 if pages == "Short":
-    df_pages = df_genre[df_genre['Pages'] <= 299]
+    df_pages = df_genre[df_genre["Pages"] <= 299]
 else:
-    df_pages = df_genre[df_genre['Pages'] >= 300]
+    df_pages = df_genre[df_genre["Pages"] >= 300]
 
 #Filter 3
-ratings_count = st.sidebar.selectbox('Would you like to read a popular book or a hidden gem?',
-                                     ('Popular', 'Hidden gem'))
+ratings_count = st.sidebar.selectbox("Would you like to read a popular book or a hidden gem?",
+                                     ("Popular", "Hidden gem"))
 if ratings_count == "Hidden gem":
-    df_ratings_count = df_pages[df_pages["Ratings count"] <= 50000]
+    df_ratings_count = df_pages[df_pages["Ratings count"] <= 100000]
 else:
-    df_ratings_count = df_pages[df_pages["Ratings count"] >= 50001]                                    
+    df_ratings_count = df_pages[df_pages["Ratings count"] >= 100001]                                    
 
 #Filter 4
-published = st.sidebar.selectbox('Do you prefer to read a recent book or a classic?',
-                                     ('Recent', 'Classic'))
+published = st.sidebar.selectbox("Do you prefer to read a recent book or a classic?",
+                                     ("Recent", "Classic"))
 if published == "Classic":
-    df_published = df_ratings_count[df_ratings_count['First published'] <= 2017]
+    df_published = df_ratings_count[df_ratings_count["First published"] <= 2017]
 else:
-    df_published = df_ratings_count[df_ratings_count['First published'] >= 2018]
+    df_published = df_ratings_count[df_ratings_count["First published"] >= 2018]
 
 df_sample = df_published.sample(n=3).reset_index()
 
