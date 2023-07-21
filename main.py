15b7a97fe002bf74a8bc070b88ce977ce39f941d
Margarita Montenegro <margamontv@gmail.com>
2023-07-21 22:20:51 +0200

    project_finished

--- main.py
+++ main.py
@@ -1,84 +1,53 @@
+#Imports
 import streamlit as st
 import pandas as pd
-import numpy as np
-from PIL import Image
+from p_analysis import m_analysis as man
 
-st.set_page_config(
-        page_title="LitPick - App for book recommendation",
-)
+#Call the function to load the CSS
+man.load_css()
 
-# Cargar el CSS personalizado
-def load_css():
-    with open('./css/style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+#Read the csv with the scraped and cleaned books
+books_df = man.read_csv('./data/books_clean.csv')
 
-# Llamar a la función para cargar el CSS y el toml
-load_css()
-
-books_df = pd.read_csv('./data/books_clean.csv')
-
-# Load your custom design image
+#Load the custom design image
 title_image = "./book_logo_title.jpg"
 
-# Display the image as the app title
+#Display the image as the app title and the title
 st.image(title_image)
-st.title("LitPick - App for book recommendation")
 
+#Give instructions to the user of how the app works
 st.markdown("""
+ * This is an app for book recommendations
  * Use the left menu to select your books preferences
  * Your recommendations will appear below, You can click on the cover to see the whole book info
 """)
 
-#Filter 1
-genre = st.sidebar.selectbox(
-    'Which genre would you like to read?',
-    ("Adult Fiction", "Biography", "Business", "Chick-Lit", "Childrens", 
-              "Comics", "Contemporary", "Crime", "Fantasy", "Fiction", 
-              "Graphic Novels", "Historical Fiction", "History", "Horror", "Humor", 
-              "LGBT", "Literary Fiction", "Memoir", "Mystery", "New Adult", 
-              "Nonfiction", "Paranormal", "Poetry", "Romance", "Science", 
-              "Science Fiction", "Self-Help", "Suspense", "Thriller", "Travel", "Young Adult"))
-df_genre = books_df[books_df["Genre"] == genre]
+#Book Filter 1 by genre
+df_genre = man.filter_genre(books_df)
 
-#Filter 2
-pages = st.sidebar.selectbox("Would you like to read a short book or a long book?",
-                             ("Short", "Long"))
-if pages == "Short":
-    df_pages = df_genre[df_genre["Pages"] <= 299]
-else:
-    df_pages = df_genre[df_genre["Pages"] >= 300]
+#Book Filter 2 by pages
+df_pages = man.filter_pages(df_genre)
 
-#Filter 3
-ratings_count = st.sidebar.selectbox("Would you like to read a popular book or a hidden gem?",
-                                     ("Popular", "Hidden gem"))
-if ratings_count == "Hidden gem":
-    df_ratings_count = df_pages[df_pages["Ratings count"] <= 100000]
-else:
-    df_ratings_count = df_pages[df_pages["Ratings count"] >= 100001]                                    
+#Book Filter 3 by ratings count
+df_ratings_count = man.filter_ratings_count(df_pages)                                 
 
-#Filter 4
-published = st.sidebar.selectbox("Do you prefer to read a recent book or a classic?",
-                                     ("Recent", "Classic"))
-if published == "Classic":
-    df_published = df_ratings_count[df_ratings_count["First published"] <= 2017]
-else:
-    df_published = df_ratings_count[df_ratings_count["First published"] >= 2018]
+#Book Filter 4 by year published
+df_published = man.filter_published(df_ratings_count)
 
-df_sample = df_published.sample(n=3).reset_index()
+#Create a sample of 3 books
+df_sample = man.sample_df(df_published)
 
+#Create columns so the recommendations will appear side by side
 col1, col2, col3 = st.columns(3)
 
-def show_images(df):
-    for index, row in df.iterrows():
-        st.markdown("[![Imagen](" + row["Cover image"] + ")]("+row["Book url"]+")")
-        st.markdown(row["Title"] + " " + "by" + " " + row["Author"])
-
+#Apply to the columns the function to link the cover image with the url
 with col1:
-    show_images(pd.DataFrame(df_sample.iloc[[0]]))
+    man.show_images(pd.DataFrame(df_sample.iloc[[0]]))
 with col2:
-    show_images(pd.DataFrame(df_sample.iloc[[1]]))
+    man.show_images(pd.DataFrame(df_sample.iloc[[1]]))
 with col3:
-    show_images(pd.DataFrame(df_sample.iloc[[2]]))
+    man.show_images(pd.DataFrame(df_sample.iloc[[2]]))
 
+#Include a shuffle button for the user
 st.button("Shuffle")
 
---
+++ p_analysis/m_analysis.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+# Cargar el CSS personalizado
+def load_css():
+    with open('./css/style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#Read the csv with the scraped and cleaned books
+def read_csv(path):
+    return pd.read_csv(path)
+
+#Book Filter 1 by genre
+def filter_genre(df_1):
+    genre = st.sidebar.selectbox(
+    'Which genre would you like to read?',
+    ("Adult Fiction", "Biography", "Business", "Chick-Lit", "Childrens", 
+              "Comics", "Contemporary", "Crime", "Fantasy", "Fiction", 
+              "Graphic Novels", "Historical Fiction", "History", "Horror", "Humor", 
+              "LGBT", "Literary Fiction", "Memoir", "Mystery", "New Adult", 
+              "Nonfiction", "Paranormal", "Poetry", "Romance", "Science", 
+              "Science Fiction", "Self-Help", "Suspense", "Thriller", "Travel", "Young Adult"))
+    
+    return df_1[df_1["Genre"] == genre]
+
+#Book Filter 2 by pages
+def filter_pages(df_2):
+    pages = st.sidebar.selectbox("Would you like to read a short book or a long book?",
+                             ("Short", "Long"))
+    if pages == "Short":
+        return df_2[df_2["Pages"] <= 299]
+    else:
+        return df_2[df_2["Pages"] >= 300]
+
+#Book Filter 3 by ratings count
+def filter_ratings_count(df_3):
+    ratings_count = st.sidebar.selectbox("Would you like to read a popular book or a hidden gem?",
+                                     ("Popular", "Hidden gem"))
+    if ratings_count == "Hidden gem":
+        return df_3[df_3["Ratings count"] <= 100000]
+    else:
+        return df_3[df_3["Ratings count"] >= 100001]                                   
+
+#Book Filter 4 by year published
+def filter_published(df_4):
+    published = st.sidebar.selectbox("Do you prefer to read a recent book or a classic?",
+                                     ("Recent", "Classic"))
+    if published == "Classic":
+        return df_4[df_4["First published"] <= 2017]
+    else:
+        return df_4[df_4["First published"] >= 2018]
+
+#Create a sample of 3 books
+def sample_df(df_s):
+    return df_s.sample(n=3).reset_index()
+
+#Create columns so the recommendations will appear side by side
+col1, col2, col3 = st.columns(3)
+
+#Function to link the cover image with the url
+def show_images(df):
+    for index, row in df.iterrows():
+        st.markdown("[![Imagen](" + row["Cover image"] + ")]("+row["Book url"]+")")
+        st.markdown(row["Title"] + " " + "by" + " " + row["Author"])
+
